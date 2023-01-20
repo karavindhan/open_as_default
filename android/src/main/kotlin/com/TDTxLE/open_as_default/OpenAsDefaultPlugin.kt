74aab49e239217becfb5dd6d055400cc6f630715
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-01-20 07:56:19 +0000

    cleanup OpenAsDefaultPlugin.kt
    https://github.com/adil192/saber/issues/255

--- android/src/main/kotlin/com/TDTxLE/open_as_default/OpenAsDefaultPlugin.kt
+++ android/src/main/kotlin/com/TDTxLE/open_as_default/OpenAsDefaultPlugin.kt
@@ -13,10 +13,6 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
-
-
-
-
 /** OpenAsDefaultPlugin */
 class OpenAsDefaultPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
@@ -24,28 +20,17 @@ class OpenAsDefaultPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
-  var uriToUse: String? = ""
-  private var binding: ActivityPluginBinding? = null
+
+  var fileIntentUri: String? = null
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-
-
-
-
-
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "open_as_default")
     channel.setMethodCallHandler(this)
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "getFileIntent") {
-
-      val batteryLevel = uriToUse
-      if (batteryLevel != null || batteryLevel!="") {
-        result.success(batteryLevel)
-      } else {
-        result.error("UNAVAILABLE", "Battery level not available.", null)
-      }
+      result.success(fileIntentUri)
     } else {
       result.notImplemented()
     }
@@ -56,41 +41,27 @@ class OpenAsDefaultPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   }
 
   override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    this.binding = binding
     //binding.addOnNewIntentListener(this)
 
-
-
-    // ::::::::::::: validar si es que llego algun archivo :::::::::::::::::::
     val parameters = binding.activity.intent.extras
-    if (parameters != null) uriToUse = parameters.getString("uri", "")
+    if (parameters != null) fileIntentUri = parameters.getString("uri", "")
 
     try {
       val intent = binding.activity.intent
-      if (intent != null && intent.data != null && intent.data.toString()
-                      .contains("content://")
-      ) {
-        uriToUse = intent.data.toString()
-        //println(uriToUse)
+      val data: String? = intent?.data?.toString()
+      if (data != null && data.contains("content://")) {
+        fileIntentUri = intent.data.toString()
       }
-    } catch (e: Exception) {
-      uriToUse = ""
-    }
-    // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
+    } catch (e: Exception) {}
   }
 
   override fun onDetachedFromActivityForConfigChanges() {
-    TODO("Not yet implemented")
   }
 
   override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    TODO("Not yet implemented")
   }
 
   override fun onDetachedFromActivity() {
-    TODO("Not yet implemented")
+    fileIntentUri = null
   }
-
-
 }
